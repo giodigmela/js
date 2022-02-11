622d2d8dfaf11ca9be2bb6311d4c801d96ff01ea
giorgi20016 <87188095+HotelGareji@users.noreply.github.com>
2022-02-11 20:18:04 +0400

    js_1

---
+++ index.js
@@ -0,0 +1,34 @@
+var text_box = document.getElementById("object");
+var button = document.getElementById("button");
+var red = document.getElementById("red");
+var green = document.getElementById("green");
+var yellow = document.getElementById("yellow");
+
+function changecolor(color) {
+    object.style.background = color;
+}
+
+button.onclick = function(){
+
+    if(object.className == "appear"){
+        object.className = "";
+        button.innerHTML = "appear";
+    } else{
+        object.className = "appear";
+        button.innerHTML = "hide";
+    }
+
+};
+
+red.onclick =  function(){
+    changecolor("rgb(204, 7, 7)");
+};
+
+green.onclick =  function(){
+    changecolor("green");
+};
+
+yellow.onclick =  function(){
+    changecolor("yellow");
+};
+
